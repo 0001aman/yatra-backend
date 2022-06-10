351aef39f1adef1ae9c84d4ae364e74632e61e2c
aman <amanvinod23@gmail.com>
2022-06-10 13:09:48 +0530

    added models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Destination(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="DIMAGES")
+    about = models.TextField()
+    video = models.CharField(max_length=1000)
+
+
+class ArtForms(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="AIMAGES")
+    about = models.TextField()
+    video = models.CharField(max_length=1000)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="IMAGES")
+    about = models.TextField()
+    speciality = models.TextField()
+    coustomisation = models.BooleanField()
